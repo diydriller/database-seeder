9e35947f1c5905232d4cfa363cef254313c50b8a
GitHub Action <action@github.com>
2025-10-05 23:30:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/diydriller/databaseseeder/MyBundle.kt
+++ src/main/kotlin/com/github/diydriller/databaseseeder/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.diydriller.databaseseeder
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/diydriller/databaseseeder/services/MyProjectService.kt
+++ src/main/kotlin/com/github/diydriller/databaseseeder/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.diydriller.databaseseeder.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.diydriller.databaseseeder.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
