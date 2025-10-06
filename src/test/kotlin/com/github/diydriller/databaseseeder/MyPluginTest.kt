diydriller/ai-sql-translator

Template cleanup

src/test/kotlin/com/github/diydriller/aisqltranslator/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.diydriller.aisqltranslator
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.diydriller.aisqltranslator.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {

diydriller/database-seeder

src/test/kotlin/com/github/diydriller/databaseseeder/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.diydriller.databaseseeder
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.diydriller.databaseseeder.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {